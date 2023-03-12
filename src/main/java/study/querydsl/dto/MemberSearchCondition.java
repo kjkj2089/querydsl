da6a0bc58015bdadb09c3a1d1fa2ba8c8dcc31e1
kjkj2089 <62068368+kjkj2089@users.noreply.github.com>
2023-03-12 23:05:35 +0900

    queryDsl 중급문법, 실무활용 - 순수 JPA와 QueryDsl

---
+++ src/main/java/study/querydsl/dto/MemberSearchCondition.java
@@ -0,0 +1,22 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberSearchCondition {
+    //회원명, 팀명, 나이(ageGoe, ageLoe)
+
+    private String username;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+
+    public MemberSearchCondition(String username, String teamName, Integer ageGoe, Integer ageLoe) {
+        this.username = username;
+        this.teamName = teamName;
+        this.ageGoe = ageGoe;
+        this.ageLoe = ageLoe;
+    }
+}
